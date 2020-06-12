948e55d1b3d37302937959bbbccb43886ec73740
matt-farmer <solversurfer@icloud.com>
2020-06-12 22:00:07 +1000

    intial commit

--- align.go
+++ align.go
@@ -1,7 +1,49 @@
 package otfalign
 
-import "github.com/labstack/echo"
+import (
+	"fmt"
 
-type OtfAligner struct {
-	srvr *echo.Echo
+	"github.com/labstack/echo"
+)
+
+type OtfAlignService struct {
+	srvr        *echo.Echo
+	serviceName string
+	serviceID   string
+	serviceHost string
+	servicePort int
+	niasHost    string
+	niasPort    int
+	tcHost      string
+	tcPort      int
+}
+
+//
+// create a new service instance
+//
+func New(options ...Option) (*OtfAlignService, error) {
+
+	srvc := OtfAlignService{}
+
+	if err := srvc.setOptions(options...); err != nil {
+		return nil, err
+	}
+
+	return &srvc, nil
+}
+
+func (s *OtfAlignService) PrintConfig() {
+
+	fmt.Println("\n\tOTF-Align Service Configuration")
+	fmt.Println("\t---------------------------------\n")
+
+	s.printID()
+	// rdr.printNiasConfig()
+	// rdr.printClassifierConfig()
+
+}
+
+func (s *OtfAlignService) printID() {
+	fmt.Println("\talign service name:\t\t", s.serviceName)
+	fmt.Println("\talign service ID:\t\t", s.serviceID)
 }
